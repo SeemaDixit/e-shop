8381ca72d88323e54ab9e2cf0f815ed9afdc29e9
Seema Dixit <seema.dixitv@gmail.com>
2021-01-21 15:07:30 -0800

    Homepage component

---
+++ src/homepage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './homepage.scss';
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='sub-title'>SHOP NOW</span>
+
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>JACKETS</h1>
+                        <span className='sub-title'>SHOP NOW</span>
+
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>MEN</h1>
+                        <span className='sub-title'>SHOP NOW</span>
+
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>WOMAN</h1>
+                        <span className='sub-title'>SHOP NOW</span>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Homepage;
